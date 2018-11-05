ff4b5e4fd75cc96193ce4cf1afeb5b1296f0b5fe
Daniel Kravchenko <danil180401@gmail.com>
2018-11-05 16:58:21 +0200

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+    
+    before_action :set_post, only:[ :show, :edit, :update, :destroy]
+
+
+    
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        # @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
+    end
+
+    def edit 
+        # @post = Post.find(params[:id])
+    end
+
+    def update
+        # @post = Post.find(params[:id])
+        if @post.update_attributes(post_params)
+            redirect_to @post
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        # @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+    
+    private
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :summary, :body)
+    end
+    
+end
